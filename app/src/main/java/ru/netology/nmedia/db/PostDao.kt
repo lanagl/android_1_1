53d1f49bbd5f8aebcf71f7efbc464041901627c8
lanagl <lanaglaz@gmail.com>
2022-07-11 16:48:11 +0300

    4.2 Библиотека Room

--- app/src/main/java/ru/netology/nmedia/db/PostDao.kt
+++ app/src/main/java/ru/netology/nmedia/db/PostDao.kt
@@ -1,10 +1,40 @@
 package ru.netology.nmedia.db
 
-import ru.netology.nmedia.Post
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
+@Dao
 interface PostDao {
-    fun getAll(): List<Post>
-    fun save(post: Post): Post
+    @Query("SELECT * FROM posts ORDER BY id DESC")
+    fun getAll(): LiveData<List<PostEntity>>
+
+    @Insert
+    fun insert(post: PostEntity)
+
+    @Query("UPDATE posts SET text= :content WHERE id = :id")
+    fun updateContentById(id: Long, content: String)
+
+    @Query(
+        """
+            UPDATE posts SET 
+            likesCount = likesCount + CASE WHEN likeByMe THEN -1 ELSE 1 END,
+            likeByMe = CASE WHEN likeByMe THEN 0 ELSE 1 END
+            WHERE id=:id
+        """
+    )
     fun likeById(id: Long)
+
+    @Query(
+        """
+                  UPDATE posts SET 
+            reposts = reposts + 1
+            WHERE id=:id      
+        """
+    )
+    fun shareById(id: Long)
+
+    @Query("DELETE FROM posts WHERE id=:id")
     fun removeById(id: Long)
 }
--- app/src/main/java/ru/netology/nmedia/db/Posts.kt
+++ app/src/main/java/ru/netology/nmedia/db/Posts.kt
@@ -1,17 +1,28 @@
 package ru.netology.nmedia.db
 
-import android.database.Cursor
 import ru.netology.nmedia.Post
 
 
-fun Cursor.toPost()= Post(
-    id = getLong(getColumnIndexOrThrow(PostsTable.ColumnPosts.ID.columnName)),
-    author = getString(getColumnIndexOrThrow(PostsTable.ColumnPosts.AUTHOR.columnName)),
-    text = getString(getColumnIndexOrThrow(PostsTable.ColumnPosts.TEXT.columnName)),
-    date = getLong(getColumnIndexOrThrow(PostsTable.ColumnPosts.DATE.columnName)),
-    views = getInt(getColumnIndexOrThrow(PostsTable.ColumnPosts.VIEWS.columnName)),
-    reposts = getInt(getColumnIndexOrThrow(PostsTable.ColumnPosts.REPOSTS.columnName)),
-    likesCount = getInt(getColumnIndexOrThrow(PostsTable.ColumnPosts.LIKESCOUNT.columnName)),
-    likeByMe = getInt(getColumnIndexOrThrow(PostsTable.ColumnPosts.LIKE_BY_ME.columnName)) != 0,
-    video = getString(getColumnIndexOrThrow(PostsTable.ColumnPosts.VIDEO.columnName)),
+internal fun PostEntity.toModel()= Post(
+    id = id,
+    author = author,
+    text = text,
+    date = date,
+    views = views,
+    reposts = reposts,
+    likesCount = likesCount,
+    likeByMe = likeByMe,
+    video = video,
+)
+
+internal fun Post.toEntity()= PostEntity(
+    id = id,
+    author = author,
+    text = text,
+    date = date,
+    views = views,
+    reposts = reposts,
+    likesCount = likesCount,
+    likeByMe = likeByMe,
+    video = video,
 )
